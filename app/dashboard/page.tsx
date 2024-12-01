0ae7b479ea9d3e094cb8966f1608e2b89645d58c
Josh Bucsit <bucsitjosuejr@gmail.com>
2024-12-01 09:18:38 +0800

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// export default function Page() {
+//   return <p>Dashboard Page</p>;
+// }
+import React from "react";
+
+const Page = () => {
+  return <div>Dashboard Page</div>;
+};
+
+export default Page;
